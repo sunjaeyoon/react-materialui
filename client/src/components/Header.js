3d364236cfa7a3f401edb9eef523f92d29be8a60
Tim <dannyho1238D@gmail.com>
2021-01-06 18:57:31 -1000

    Added a Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,9 +6,6 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
@@ -29,10 +26,7 @@ export default function Header() {
     const [auth, setAuth] = React.useState(true);
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
-  
-    const handleChange = (event) => {
-      setAuth(event.target.checked);
-    };
+
   
     const handleMenu = (event) => {
       setAnchorEl(event.currentTarget);
@@ -44,12 +38,6 @@ export default function Header() {
   
     return (
       <div className={classes.root}>
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-            label={auth ? 'Logout' : 'Login'}
-          />
-        </FormGroup>
         <AppBar position="static">
           <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
