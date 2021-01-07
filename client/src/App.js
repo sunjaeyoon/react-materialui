11500a3e6f0c50db044ffbe6a6698e1dd2642caf
Tim <dannyho1238D@gmail.com>
2021-01-06 18:48:09 -1000

    first

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      hello
+      {/*Header*/}
+      {/*Body
+          left middle right
+      */}
+    </div>
+  );
+}
+
+export default App;
