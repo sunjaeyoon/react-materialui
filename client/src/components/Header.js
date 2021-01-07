835cb8f3c9f9f33d535faff894d0de309774b307
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2021-01-07 03:20:12 -0500

    custom nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,126 +1,22 @@
 import React from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import HomeIcon from '@material-ui/icons/Home';
-import ScheduleRoundedIcon from '@material-ui/icons/ScheduleRounded';
-import Button from '@material-ui/core/Button';
+import "./Header.css"
 
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-    },
-  },
-});
-
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-
-export default function Header() {
-    const classes = useStyles();
-    const [auth, setAuth] = React.useState(true);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-
-  
-    const handleMenu = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    return (
-      <div className={classes.root}>
-        <ThemeProvider theme={theme}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Photos
-            </Typography>
-            
-            <Button color="secondary">
-              Home
-            </Button>
-            <Button color="secondary">
-              About
-            </Button>
-            <Button color="secondary">
-              Something
-            </Button>
-            
-            <IconButton>
-              <HomeIcon/>
-            </IconButton>
-            <IconButton>
-              <ScheduleRoundedIcon/>
-            </IconButton>
-            {auth && (
-              <div>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-        </ThemeProvider>
+const Header = () => {
+  return (
+    <div className="header_main">
+      <div className="header_right">
+        <Typography variant="h4">Something right here</Typography>
       </div>
-    );
-  }
+      <div className="header_left">
+        <Typography variant="h5">Something 1</Typography>
+        <Typography variant="h5">Something 2</Typography>
+        <Typography variant="h5">Something 3</Typography>
+        <Typography variant="h5">Something 4</Typography>
+      </div>
+    </div>
+  )
+}
+
+export default Header
+
